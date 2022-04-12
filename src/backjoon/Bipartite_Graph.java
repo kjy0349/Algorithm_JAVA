1008a062fcfc382a0f29b0a45354ffffa08a3884
kjy0349 <kjy0349@naver.com>
2022-04-12 18:14:19 +0900

    Bipartite_Graph solved

--- src/backjoon/Bipartite_Graph.java
+++ src/backjoon/Bipartite_Graph.java
@@ -3,32 +3,33 @@ import java.util.*;
 import java.io.*;
 
 public class Bipartite_Graph {
-    static ArrayList<Integer> first = new ArrayList<>();
-    static ArrayList<Integer> second = new ArrayList<>();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void bfs(boolean[] a, int start, ArrayList<ArrayList<Integer>> adjacency_list) {
+
+    public static boolean solve(ArrayList<ArrayList<Integer>> adjacency_list, int[] color, int start) {
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
-        a[start] = true;
         while (!q.isEmpty()) {
             int x = q.poll();
+            if (color[x] == 0) color[x] = 1;
             for (int elem : adjacency_list.get(x)) {
-                if (!a[elem]) {
+                if (color[elem] == 0) {
                     q.add(elem);
-                    a[elem] = true;
-                    second.add(elem);
+                    color[elem] = 3 - color[x];
                 }
+                else if (color[x] == color[elem]) return false;
             }
         }
+        return true;
     }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
-        for (int i = 0;i < K;i++){
+        for (int i = 0;i < K;i++) {
             String[] inputs = br.readLine().split(" ");
             int V = Integer.parseInt(inputs[0]);
             int E = Integer.parseInt(inputs[1]);
-            boolean[] a = new boolean[V + 1];
+            boolean is_possible = true;
             ArrayList<ArrayList<Integer>> adjacency_list = new ArrayList<>();
             for (int j = 0;j < V + 1;j++)adjacency_list.add(new ArrayList<>());
             for (int j = 0;j < E;j++) {
@@ -36,16 +37,18 @@ public class Bipartite_Graph {
                 int from = Integer.parseInt(inputs[0]);
                 int to = Integer.parseInt(inputs[1]);
                 adjacency_list.get(from).add(to);
+                adjacency_list.get(to).add(from);
             }
-            first.clear();
-            second.clear();
+            int[] color = new int[V + 1];
             for (int j = 1;j < V + 1;j++) {
-                if (!a[j]) {
-                    first.add(j);
-                    bfs(a, j, adjacency_list);
+                if (color[j] == 0) {
+                    if (!solve(adjacency_list, color, j)) {
+                        is_possible = false;
+                        break;
+                    }
                 }
             }
-            if (first.size() > 1 && second.size() > 0) bw.write("YES\n");
+            if (is_possible) bw.write("YES\n");
             else bw.write("NO\n");
         }
         bw.flush();
