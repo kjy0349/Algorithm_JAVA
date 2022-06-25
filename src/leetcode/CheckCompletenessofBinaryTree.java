9a77da7cc08c91ecc414b5bda1242662150c350e
kjy0349 <kjy0349@naver.com>
2022-06-25 18:20:34 +0900

    Tree,Seq

---
+++ src/leetcode/CheckCompletenessofBinaryTree.java
@@ -0,0 +1,46 @@
+package leetcode;
+import java.util.*;
+
+class CheckCompletenessofaBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static ArrayList<Integer> arr;
+    public void bfs(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            arr.add(temp.val);
+            if (temp.left != null) queue.add(temp.left);
+            else if (temp.left == null && temp.val != -1) queue.add(new TreeNode(-1, null, null));
+            if (temp.right != null) queue.add(temp.right);
+            else if (temp.right == null && temp.val != -1) queue.add(new TreeNode(-1, null, null));
+        }
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        if (root.left == null && root.right == null) return true;
+        arr = new ArrayList<>();
+        bfs(root);
+        boolean is_poss = true;
+        for (int elem : arr) {
+            if (elem == -1) {
+                if (is_poss) {
+                    is_poss = false;
+                }
+            }
+            if (!is_poss && elem != -1) return false;
+        }
+        return true;
+    }
+}
