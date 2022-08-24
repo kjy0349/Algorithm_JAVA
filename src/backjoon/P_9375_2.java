c661e3b93fa990949622df9af603e54aacc570b6
kjy0349 <kjy0349@naver.com>
2022-08-24 19:42:47 +0900

    08-24

--- src/backjoon/P_9375_2.java
+++ src/backjoon/P_9375_2.java
@@ -1,31 +1,18 @@
 package backjoon;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class P_9375 {
-    static long answer;
+public class P_9375_2 {
     static HashMap<String, ArrayList<String>> clothes;
     static ArrayList<String> types;
-
-    public static void comb(int start) {
-        if (start != types.size()) { // 옷 종류 String들을 갖고 있는 문자열 ArrayList
-            for (int i = start; i < types.size(); i++) { // 30
-                int size = clothes.get(types.get(i)).size(); // 1
-                for (int j = 0; j < size; j++) {
-                    answer++;
-                    comb(i + 1);
-                }
-            }
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
-            answer = 0; // 횟수 세서 반환할 변수
             clothes = new HashMap<>(); // 타입, 옷 이름들을 갖고 있는 ArrayList
             types = new ArrayList<>();
             for (int j = 0; j < N; j++) {
@@ -37,8 +24,9 @@ public class P_9375 {
                     clothes.get(inputs[1]).add(inputs[0]);
                 }
             }
-            comb(0);
-            bw.write(answer + "\n");
+            int cnt = 1;
+            for (String elem : types) cnt *= clothes.get(elem).size() + 1;
+            bw.write(cnt - 1 + "\n");
         }
         bw.flush();
     }
--- src/backjoon/P_9461.java
+++ src/backjoon/P_9461.java
@@ -1,7 +1,21 @@
 package backjoon;
 
-public class P_9461 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class P_9461 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long[] d = new long[101];
+        d[1] = 1;
+        d[2] = 1;
+        d[3] = 1;
+        for (int i = 4; i < d.length; i++) d[i] = d[i - 2] + d[i - 3];
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(d[N]);
+        }
     }
 }
