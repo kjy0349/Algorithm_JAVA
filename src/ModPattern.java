dd739c3fd67e83ce6839edba3c1bdfd0695c7554
kjy0349 <kjy0349@naver.com>
2023-02-17 22:21:16 +0900

    Circle distance

--- src/ModPattern.java
+++
@@ -1,9 +0,0 @@
-public class ModPattern {
-    public static void main(String[] args) {
-        long num = 1;
-        for (int i = 0; i < 100; i++) {
-            num *= 10;
-            System.out.println(num + " " + num % 7);
-        }
-    }
-}
---
+++ src/study_42/P_1002.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class P_1002 {
+    static class Circle {
+        int x;
+        int y;
+        int r;
+        Circle (int x, int y, int r) {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Circle fir = new Circle(inputs[0], inputs[1], inputs[2]);
+            Circle sec = new Circle(inputs[3], inputs[4], inputs[5]);
+            double dist = Math.sqrt(Math.pow(fir.x - sec.x, 2) + Math.pow(fir.y - sec.y, 2));
+            if (dist == 0 && fir.r == sec.r) bw.write("-1\n");
+            else if (dist == 0) bw.write("0\n");
+            else if (fir.r + sec.r > dist && Math.abs(fir.r - sec.r) < dist) bw.write("2\n");
+            else if (fir.r + sec.r < dist || Math.abs(fir.r - sec.r) > dist) bw.write("0\n");
+            else if (fir.r + sec.r == dist || Math.abs(fir.r - sec.r) == dist) bw.write("1\n");
+            else bw.write("0\n");
+        }
+        bw.flush();
+    }
+}
