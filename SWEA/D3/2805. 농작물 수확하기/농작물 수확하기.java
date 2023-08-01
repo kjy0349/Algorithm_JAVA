580fc6a489a7f283788f363798c9969aca88ac1c
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2023-08-01 15:41:45 +0900

    [D3] Title: 농작물 수확하기, Time: 134 ms, Memory: 20,152 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -4,19 +4,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-
 public class Solution {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
+		int[][] farm = new int[50][50];
 		StringBuilder sb = new StringBuilder();
 		for (int test = 1; test <= T; test++) {
 			int N = Integer.parseInt(br.readLine());
-			int[][] farm = new int[N][N];
 			for (int i = 0; i < N; i++) {
-				farm[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+				String line = br.readLine();
+				for (int j = 0; j < N; j++) farm[i][j] = line.charAt(j) - '0';
 			}
 			int i, j;
 			int answer = 0;
