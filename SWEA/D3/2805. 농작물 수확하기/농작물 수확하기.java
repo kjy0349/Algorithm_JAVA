4c6765409192bbbd2d217df3d3289cdd645f56c1
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2023-08-01 15:37:10 +0900

    [D3] Title: 농작물 수확하기, Time: 256 ms, Memory: 37,812 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int test = 1; test <= T; test++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] farm = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				farm[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+			}
+			int i, j;
+			int answer = 0;
+			for (int row = 0; row < N; row++) {
+				int idx = 0;
+				i = j = N / 2;
+				answer += farm[row][j];
+				if (row <= N / 2) {
+					while (idx < row) {
+						i--;
+						j++;
+						answer += farm[row][i];
+						answer += farm[row][j];
+						idx++;
+					}					
+				} else {
+					while (idx < N - row - 1) {
+						i--;
+						j++;
+						answer += farm[row][i];
+						answer += farm[row][j];
+						idx++;
+					}
+				}
+			}
+			sb.append("#");
+			sb.append(test);
+			sb.append(" ");
+			sb.append(answer);
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
