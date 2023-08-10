f2c7e3dadc53f187a341a741fc957401c4dde786
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2023-08-10 16:13:16 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1220 ms, Memory: 51116 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,58 +1,43 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static boolean[][] visited;
-    static int[][] arr;
-    static int all_count;
-    public static void find_sol(int x, int y, int dir, int count, int before) {
-        if (count != all_count) {
-            if (dir == 4) {
-                dir = 0;
-                x++;
-                y++;
-                if (x < arr.length && y < arr[0].length &&!visited[x][y]) find_sol(x, y, dir, count, arr[x][y]);
-            } else {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
-                if (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    int tmp = arr[nx][ny];
-                    arr[nx][ny] = before;
-                    before = tmp;
-                    find_sol(nx, ny, dir, count + 1, before);
-                } else find_sol(x, y, dir + 1, count, before);
-            }
-        }
-    }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        all_count = N * M;
-        visited = new boolean[N][M];
+        int[][] array = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) array[i][j] = Integer.parseInt(st.nextToken());
         }
-        for (int k = 0; k < R; k++) {
-            find_sol(0, 0, 0, 0, arr[0][0]);
-            for (int i = 0; i < visited.length; i++)  {
-                for (int j = 0; j < visited[0].length; j++) visited[i][j] = false;
-            }
+        int gr_cnt = Math.min(N, M) / 2;
+        ArrayList<ArrayList<Integer>> groups = new ArrayList<>();
+        for (int i = 0; i < gr_cnt; i++) {
+        	ArrayList<Integer> arr = new ArrayList<>();
+        	for (int j = i; j <= M - 2 - i; j++) arr.add(array[i][j]);
+        	for (int j = i; j <= N - 2 - i; j++) arr.add(array[j][M - 1 - i]);
+        	for (int j = M - 1 - i; j > i; j--) arr.add(array[N - 1 - i][j]);
+        	for (int j = N - 1 - i; j > i; j--) arr.add(array[j][i]);
+        	groups.add(arr);
         }
-        for (int[] ints : arr) {
-            for (int j = 0; j < arr[0].length - 1; j++) bw.write(ints[j] + " ");
-            bw.write(Integer.toString(ints[arr[0].length - 1]));
-            bw.newLine();
+        for (int i = 0; i < gr_cnt; i++) {
+        	ArrayList<Integer> elem = groups.get(i);
+        	int len = elem.size();
+        	int index = R % len;
+        	for (int j = i; j <= M - 2 - i; j++, index = (index + 1) % len) array[i][j] = elem.get(index);
+        	for (int j = i; j <= N - 2 - i; j++, index = (index + 1) % len) array[j][M - 1 - i] = elem.get(index);
+        	for (int j = M - 1 - i; j > i; j--, index = (index + 1) % len) array[N - 1 - i][j] = elem.get(index);
+        	for (int j = N - 1 - i; j > i; j--, index = (index + 1) % len) array[j][i] = elem.get(index);
         }
-        bw.flush();
+        for (int i = 0; i < array.length; i++) {
+        	for (int j = 0; j < array[0].length; j++) System.out.print(array[i][j] + " ");
+        	System.out.println();
+        }
     }
 }
