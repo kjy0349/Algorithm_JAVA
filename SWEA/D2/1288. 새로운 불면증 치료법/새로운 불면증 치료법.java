90b361e699e863e7140adbe69e5b44381d51c55b
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2023-08-12 10:45:59 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 99 ms, Memory: 18,364 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,27 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        int target = 0b1111111111;
+        int target = (1 << 10) - 1; // 모든 숫자가 등장했을 때의 값
         for (int test = 1; test <= T; test++) {
             int N = Integer.parseInt(br.readLine());
-            int t = 0;
+            int t = 0; // N씩 더해가며 연산해야하므로 N을 유지해야 함. 따라서 대신 t로 사용
             int subBit = 0;
             do {
                 t += N;
-                String elem = Integer.toString(t);
+                String elem = Integer.toString(t); // 각 자릿수를 하나씩 뽑기위해 String으로 변환
                 for (int i = 0; i < elem.length(); i++) {
                     int bit = elem.charAt(i) - '0';
-                    subBit |= (1 << bit);
+                    subBit |= (1 << bit); // 각 자릿수에 해당하는 bit를 1로 변경
                 }
-            } while (target != subBit);
+            } while (target != subBit); // 모든 bit가 1인지 target과 비교
             sb.append("#").append(test).append(" ").append(t).append("\n");
         }
-        System.out.print(sb.toString());
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
