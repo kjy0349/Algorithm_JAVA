485c2884d36aca985825a4c6d2ac8bb1e678a4cb
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2023-10-03 23:41:56 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 124 ms, Memory: 14304 KB, Score: 8 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
@@ -1,24 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long[][] dp = new long[N][2]; // 0번 idx -> 안 살아. 1번 idx -> 살아., 마지막 인덱스 -> 0번에 사나 안 사나?
-        long[] nums = new long[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) nums[i] = Long.parseLong(st.nextToken());
-        dp[0][0] = nums[0];
-        // 0번에 박는 순간 1번, N - 1번 두 곳이 날아감. 0번에 박는 경우는, 1번, N - 1번 둘 다 박아야 할 때.
-        if (nums[1] != 0 || nums[N - 1] != 0) dp[0][1] = 1;
-        for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]) + nums[i];
-            dp[i][1] = dp[i - 1][0] + 1;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            long[] nums = new long[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) nums[i] = Long.parseLong(st.nextToken());
+            int cnt = 0;
+            for (int i = 0; i < N; i++) if(nums[i] == 0) cnt++;
+            if (cnt == N) {
+                System.out.println(nums.length / 2);
+                return;
+            }
+            int maxIdx = 0;
+            for (int i = 1; i < N; i++) if (nums[maxIdx] < nums[i]) maxIdx = i;
+            long[] temp = new long[N];
+            int idx = 0;
+            for (int i = maxIdx; i < N; i++) temp[idx++] = nums[i];
+            for (int i = 0; i < maxIdx; i++) temp[idx++] = nums[i];
+            int answer = 0;
+            long[] dp = new long[N];
+            answer += Arrays.stream(temp).sum();
+            for (int i = 0, j; i < N; i++) {
+                if (nums[i] > 0 || dp[i] > 0) continue;
+                for (j = i; j < N && nums[j] == 0; j++) dp[j] = 1;
+                answer += (j - i + 1) / 2;
+            }
+            System.out.println(answer);
         }
-        System.out.println(Math.max(dp[N - 1][0], dp[N - 1][1]));
-    }
 }
