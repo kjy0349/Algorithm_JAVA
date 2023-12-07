d8a3988af4b5b79f446aa22d33f8b2d7b84a4545
kjy0349 <41959969+kjy0349@users.noreply.github.com>
2023-12-07 19:10:06 +0900

    [Gold V] Title: 카드 바꾸기, Time: 156 ms, Memory: 12064 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.java
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.java
@@ -1,43 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int cntChange(int[] nums, int startValue, int diff) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != startValue) count++;
+            startValue += diff;
+        }
+        return count;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N];
+        int[] nums = new int[N + 1];
         int minAnswer = 500;
-        for (int i = 0; i < nums.length; i++) nums[i] = Integer.parseInt(st.nextToken());
-        Set<Integer> possDiff = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) nums[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                possDiff.add(nums[j] - nums[i]);
-            }
-        }
-        possDiff.add(0);
-        for (int diff : possDiff) { // diff : targetDiff. 해당 등차로 만들어야 함.
-            for (int i = 0; i < nums.length; i++) { // 중심이 될 수
-                int[] tempNums = nums.clone();
-                int subAnswer = 0;
-                for (int j = i; j < nums.length - 1; j++) {
-                    if (tempNums[j] + diff != tempNums[j + 1]) {
-                        subAnswer++;
-                        tempNums[j + 1] = tempNums[j] + diff;
-                    }
-                }
-                for (int j = i; j > 0; j--) {
-                    if (tempNums[j] - diff != tempNums[j - 1]) {
-                        subAnswer++;
-                        tempNums[j - 1] = tempNums[j] - diff;
-                    }
-                }
-                if (subAnswer < minAnswer) minAnswer = subAnswer;
+                float diff = (float)(nums[j] - nums[i]) / (j - i);
+                if (diff != (int)diff) continue;
+                minAnswer = Math.min(minAnswer, cntChange(nums, nums[i] - ((i - 1) * (int)diff), (int)diff));
             }
         }
         System.out.println(minAnswer);
